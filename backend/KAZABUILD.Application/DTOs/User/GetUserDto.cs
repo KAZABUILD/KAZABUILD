853fc964f004cdd76515ac2d59ed6396cfa954ae
KJK1KJK <83895246+KJK1KJK@users.noreply.github.com.>
2025-09-16 18:30:29 +0200

    Small user controller fixes.

--- backend/KAZABUILD.Application/DTOs/User/GetUserDto.cs
+++ backend/KAZABUILD.Application/DTOs/User/GetUserDto.cs
@@ -1,14 +1,15 @@
-using System;
-using System.Collections.Generic;
+using KAZABUILD.Domain.Enums;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KAZABUILD.Application.DTOs.User
 {
     public class GetUserDto
     {
+        //Filter By fields
+        public string? Gender { get; set; }
+        public UserRole? UserRole { get; set; }
+
+        //Paging related fields
         public bool Paging = false;
 
         [MinLength(1, ErrorMessage = "Page number must be greater than 0")]
@@ -17,8 +18,10 @@ namespace KAZABUILD.Application.DTOs.User
         [MinLength(1, ErrorMessage = "Page length must be greater than 0")]
         public int PageLength { get; set; }
 
+        //Query search string
         public string? Query { get; set; } = "";
 
+        //Sorting related fields
         public string? OrderBy { get; set; }
 
         public string SortDirection { get; set; } = "asc";
