d0ef4681d58846b5813d7e9e0da86366f21506fd
KJK1KJK <83895246+KJK1KJK@users.noreply.github.com.>
2025-09-16 19:11:57 +0200

    Created a cleanup service for logs and tokens.

--- backend/KAZABUILD.Infrastructure/DependencyInjection/RateLimitingServiceCollectionExtensions.cs
+++ backend/KAZABUILD.Infrastructure/DependencyInjection/RateLimitingServiceCollectionExtensions.cs
@@ -1,7 +1,10 @@
+using KAZABUILD.Application.Security;
+using KAZABUILD.Domain.Enums;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Security.Claims;
 using System.Threading.RateLimiting;
 
 namespace KAZABUILD.Infrastructure.DependencyInjection
@@ -11,30 +14,57 @@ namespace KAZABUILD.Infrastructure.DependencyInjection
     {
         public static IServiceCollection AddRateLimiting(this IServiceCollection services)
         {
-            //Add rate limiting middleware
+            //Add a specific rate limiting middleware for controllers
             services.AddRateLimiter(options =>
             {
                 //Limiter policy to add to controllers
                 options.AddPolicy("Fixed", context =>
-                    RateLimitPartition.GetFixedWindowLimiter(
+                {
+                    //Get user role
+                    var currentUserRole = Enum.Parse<UserRole>(context.User.FindFirstValue(ClaimTypes.Role)!);
+                    var isAdmin = RoleGroups.Admins.Contains(currentUserRole.ToString());
+
+                    //Check if user is an admin or higher
+                    if (isAdmin)
+                    {
+                        //Return unlimited limiter for admins
+                        return RateLimitPartition.GetNoLimiter("admin");
+                    }
+
+                    //Return a default limiter for other users
+                    return RateLimitPartition.GetFixedWindowLimiter(
                         partitionKey: context.Connection.RemoteIpAddress?.ToString() ?? "anon",
                         factory: _ => new FixedWindowRateLimiterOptions
                         {
                             PermitLimit = 100,
-                            Window = TimeSpan.FromMinutes(1),
+                            Window = TimeSpan.FromSeconds(10),
                             QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
                             QueueLimit = 2
-                        }));
+                        }
+                    );
+                });
 
-                //Global rate limit
+                //Enable a global rate limit for every controller
                 options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
                 {
+                    //Get user role
+                    var currentUserRole = Enum.Parse<UserRole>(httpContext.User.FindFirstValue(ClaimTypes.Role)!);
+                    var isAdmin = RoleGroups.Admins.Contains(currentUserRole.ToString());
+
+                    //Check if user is an admin or higher
+                    if (isAdmin)
+                    {
+                        //Return unlimited limiter for admins
+                        return RateLimitPartition.GetNoLimiter("admin");
+                    }
+
+                    //Return a default limiter for other users
                     return RateLimitPartition.GetFixedWindowLimiter(
                         partitionKey: httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown",
                         factory: _ => new FixedWindowRateLimiterOptions
                         {
                             PermitLimit = 100,
-                            Window = TimeSpan.FromSeconds(10),
+                            Window = TimeSpan.FromSeconds(60),
                             QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
                             QueueLimit = 2
                         });
