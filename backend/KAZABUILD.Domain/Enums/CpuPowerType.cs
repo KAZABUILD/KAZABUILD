9f30c4a0198d294e2d434be471a96911c21bc14b
JEGOLEGO <ziyadninja@gmail.com>
2025-09-30 22:13:11 +0200

    fixed a git issue with merges

---
+++ backend/KAZABUILD.Domain/Enums/CpuPowerType.cs
@@ -0,0 +1,11 @@
+namespace KAZABUILD.Domain.Enums
+{
+    /// <summary>
+    /// Enum representing different types of ports that can be connected to components.
+    /// </summary>
+    public enum CpuPowerType
+    {
+        NONE,
+        CPU_POWER,
+    }
+}
