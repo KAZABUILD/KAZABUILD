480291c8e6efcb79f378f5e53782d828e711b64f
Kopan <aakk123221@gmail.com>
2025-10-17 10:06:45 +0200

    Adding first client batch

---
+++ backend/KAZABUILD.Tests/ControllerServices/ForumPostsControllerClient.cs
@@ -0,0 +1,39 @@
+﻿namespace KAZABUILD.Tests.ControllerServices;
+
+using System.Net.Http.Json;
+using Application.DTOs.Users.ForumPost;
+
+public class ForumPostsControllerClient
+{
+    private readonly HttpClient _client;
+
+    public ForumPostsControllerClient(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<HttpResponseMessage> AddForumPost(CreateForumPostDto dto)
+    {
+        return await _client.PostAsJsonAsync("/ForumPosts/add", dto);
+    }
+
+    public async Task<HttpResponseMessage> UpdateForumPost(String forumPostId, UpdateForumPostDto dto)
+    {
+        return await _client.PutAsJsonAsync("/ForumPosts/"+forumPostId, dto);
+    }
+
+    public async Task<HttpResponseMessage> GetForumPost(String forumPostId)
+    {
+        return await _client.GetAsync("/ForumPosts/"+forumPostId);
+    }
+
+    public async Task<HttpResponseMessage> DeleteForumPost(String forumPostId)
+    {
+        return await _client.DeleteAsync("/ForumPosts/"+forumPostId);
+    }
+
+    public async Task<HttpResponseMessage> GetPosts(GetForumPostDto dto)
+    {
+        return await _client.PostAsJsonAsync("/ForumPosts/get", dto);
+    }
+}
---
+++ backend/KAZABUILD.Tests/ControllerServices/MessagesControllerClient.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Json;
+using KAZABUILD.Application.DTOs.Users.Message;
+
+namespace KAZABUILD.Tests.ControllerServices;
+
+public class MessagesControllerClient(HttpClient _client)
+{
+    public async Task<HttpResponseMessage> SendMessage(CreateMessageDto dto)
+    {
+        return await _client.PostAsJsonAsync("/Messages/add", dto);
+    }
+
+    public async Task<HttpResponseMessage> UpdateMessage(String messageId, UpdateMessageDto dto)
+    {
+        return await _client.PutAsJsonAsync("/Messages/"+messageId, dto);
+    }
+
+    public async Task<HttpResponseMessage> GetMessage(String messageId)
+    {
+        return await _client.GetAsync("/Messages/"+messageId);
+    }
+
+    public async Task<HttpResponseMessage> DeleteMessage(String messageId)
+    {
+        return await _client.DeleteAsync("/Messages/"+messageId);
+    }
+
+    public async Task<HttpResponseMessage> GetMessages(GetMessageDto dto)
+    {
+        return await _client.PostAsJsonAsync("/Messages/", dto);
+    }
+}
