3f5586ebf154f5414bc3c6a6e874a532484b06dd
Kopan <aakk123221@gmail.com>
2025-10-17 10:06:45 +0200

    Adding first client batch

--- backend/KAZABUILD.Tests/ControllerServices/AuthControllerClient.cs
+++ backend/KAZABUILD.Tests/ControllerServices/AuthControllerClient.cs
@@ -6,15 +6,10 @@ namespace KAZABUILD.Tests.ControllerServices;
 public class AuthControllerClient
 {
     private readonly HttpClient _client;
-    private readonly string? _ipAddress;
 
-    public AuthControllerClient(HttpClient client, string? ipAddress = null)
+    public AuthControllerClient(HttpClient client)
     {
         _client = client;
-        _ipAddress = ipAddress;
-
-        _client.DefaultRequestHeaders.Remove("X-Forwarded-For");
-        _client.DefaultRequestHeaders.Add("X-Forwarded-For", _ipAddress);
     }
 
     public async Task<HttpResponseMessage> Login(LoginDto dto)
@@ -24,31 +19,31 @@ public class AuthControllerClient
 
     public async Task<HttpResponseMessage> Verify2Fa(Verify2FactorAuthenticationDto dto)
     {
-        return await _client.PostAsJsonAsync("/Auth/verify-2fa", dto);
+        return await _client.PostAsJsonAsync("/Auth/login", dto);
     }
 
     public async Task<HttpResponseMessage> GoogleLogin(GoogleLoginDto dto)
     {
-        return await _client.PostAsJsonAsync("/Auth/google-login", dto);
+        return await _client.PostAsJsonAsync("/Auth/login", dto);
     }
 
     public async Task<HttpResponseMessage> Register(RegisterDto dto)
     {
-        return await _client.PostAsJsonAsync("/Auth/register", dto);
+        return await _client.PostAsJsonAsync("/Auth/login", dto);
     }
 
     public async Task<HttpResponseMessage> ConfirmRegister(ConfirmRegisterDto dto)
     {
-        return await _client.PostAsJsonAsync("/Auth/confirm-register", dto);
+        return await _client.PostAsJsonAsync("/Auth/login", dto);
     }
 
     public async Task<HttpResponseMessage> ResetPassword(ResetPasswordDto dto)
     {
-        return await _client.PostAsJsonAsync("/Auth/reset-password", dto);
+        return await _client.PostAsJsonAsync("/Auth/login", dto);
     }
 
     public async Task<HttpResponseMessage> ConfirmResetPassword(ConfirmPasswordResetDto dto)
     {
-        return await _client.PostAsJsonAsync("/Auth/confirm-reset-password", dto);
+        return await _client.PostAsJsonAsync("/Auth/login", dto);
     }
 }
