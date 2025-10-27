07ae482212540509674bd6948a41691c0e96ec92
a-kopanie <kopaniecki.adrian@gmail.com>
2025-10-27 11:12:47 +0100

    Integrating usage of DataSeeder and creating componentPricesController

--- backend/KAZABUILD.Tests/ControllerServices/AuthControllerClient.cs
+++ backend/KAZABUILD.Tests/ControllerServices/AuthControllerClient.cs
@@ -19,31 +19,31 @@ public class AuthControllerClient
 
     public async Task<HttpResponseMessage> Verify2Fa(Verify2FactorAuthenticationDto dto)
     {
-        return await _client.PostAsJsonAsync("/Auth/login", dto);
+        return await _client.PostAsJsonAsync("/Auth/verify-2fa", dto);
     }
 
     public async Task<HttpResponseMessage> GoogleLogin(GoogleLoginDto dto)
     {
-        return await _client.PostAsJsonAsync("/Auth/login", dto);
+        return await _client.PostAsJsonAsync("/Auth/google-login", dto);
     }
 
     public async Task<HttpResponseMessage> Register(RegisterDto dto)
     {
-        return await _client.PostAsJsonAsync("/Auth/login", dto);
+        return await _client.PostAsJsonAsync("/Auth/register", dto);
     }
 
     public async Task<HttpResponseMessage> ConfirmRegister(ConfirmRegisterDto dto)
     {
-        return await _client.PostAsJsonAsync("/Auth/login", dto);
+        return await _client.PostAsJsonAsync("/Auth/confirm-register", dto);
     }
 
     public async Task<HttpResponseMessage> ResetPassword(ResetPasswordDto dto)
     {
-        return await _client.PostAsJsonAsync("/Auth/login", dto);
+        return await _client.PostAsJsonAsync("/Auth/reset-password", dto);
     }
 
     public async Task<HttpResponseMessage> ConfirmResetPassword(ConfirmPasswordResetDto dto)
     {
-        return await _client.PostAsJsonAsync("/Auth/login", dto);
+        return await _client.PostAsJsonAsync("/Auth/confirm-reset-password", dto);
     }
 }
