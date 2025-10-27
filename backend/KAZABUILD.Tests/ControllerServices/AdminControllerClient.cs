f277bd1d83f9c8b881fb5ecca62366395c6daec9
Kopan <aakk123221@gmail.com>
2025-10-27 12:18:05 +0100

    Adding Admin Controller Panel

---
+++ backend/KAZABUILD.Tests/ControllerServices/AdminControllerClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Json;
+
+namespace KAZABUILD.Tests.ControllerServices;
+
+public class AdminControllerClient(HttpClient _client)
+{
+    public async Task<HttpResponseMessage> ResetSystemAdmin()
+    {
+        return await _client.PostAsJsonAsync("/Admin/reset", new {});
+    }
+
+    public async Task<HttpResponseMessage> UpdateNotification(String password)
+    {
+        return await _client.PostAsJsonAsync("/Admin/seed/"+password, new {});
+    }
+}
