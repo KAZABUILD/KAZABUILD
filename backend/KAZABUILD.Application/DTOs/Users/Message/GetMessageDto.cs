17633432565969a6dab03188f87442fda5a07e0c
KJK1KJK <83895246+KJK1KJK@users.noreply.github.com.>
2025-11-03 12:30:51 +0100

    Fixed a paging bug.

--- backend/KAZABUILD.Application/DTOs/Users/Message/GetMessageDto.cs
+++ backend/KAZABUILD.Application/DTOs/Users/Message/GetMessageDto.cs
@@ -31,13 +31,11 @@ namespace KAZABUILD.Application.DTOs.Users.Message
         /// <summary>
         /// Which page should be gotten if paging enabled.
         /// </summary>
-        [MinLength(1, ErrorMessage = "Page number must be greater than 0")]
         public int? Page { get; set; }
 
         /// <summary>
         /// How many objects should be in the response if paging enabled.
         /// </summary>
-        [MinLength(1, ErrorMessage = "Page length must be greater than 0")]
         public int? PageLength { get; set; }
 
         //Query search string
